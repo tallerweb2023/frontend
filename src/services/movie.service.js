1bce6fd9f814974c7aac0b05bb2519ddf39fce25
kevocodes <00116120@uca.edu.sv>
2023-11-23 14:31:24 -0600

    feat: add .env variables :sparkles:

--- src/services/movie.service.js
+++ src/services/movie.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:3001/api/v1";
+const BASE_URL = `${import.meta.env.VITE_API_URL}/api/v1`;
 
 export const getAllMovies = async () => {
   try {
